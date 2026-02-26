12b3763517671a10aa39f51199045e95f10e795b
Kevin Torres <ktorres@soaint.com>
2026-02-26 17:01:31 -0500

    Ajustes pendientes

--- generador_servicios.py
+++ generador_servicios.py
@@ -3103,7 +3103,7 @@ def generar_proyecto():
                                 os.path.normpath(st.session_state["ubicacion_xsd_exp"]).rsplit('.', 1)[0].replace("\\", "/"),
                                 os.path.normpath(ubicacion_proxy_destino).rsplit('.', 1)[0].replace("\\", "/")
                             )
-                            st.code(st.session_state["archivo_pipeline_exp"], language="xml")
+                            #st.code(st.session_state["archivo_pipeline_exp"], language="xml")
                             
                             
                             st.markdown(
